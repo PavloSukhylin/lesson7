781b6b8c7872feaead1e04c13f99207a4b2ca3fe
PavloSukhylin <supana1976@gmail.com>
2021-03-19 20:22:58 +0300

    init commit

---
+++ com/alevel/Main.java
@@ -0,0 +1,10 @@
+package com.alevel;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test init");
+        System.out.println("test2");
+
+
+    }
+}
